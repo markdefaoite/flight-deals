278d3557c5a05bb55e217b85046432687ce0c734
markdefaoite <markdefaoite@hotmail.com>
2024-07-01 14:54:50 -0400

    Initial Commit - Day 39 - Fetching data from Sheety API

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,28 @@
+import os
+from pprint import pprint
+
+import requests
+
+# AUTHORIZATION = os.getenv("AUTHORIZATION")
+POST_SHEETY = os.getenv("POST_SHEETY")
+
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        pass
+
+    def print_spreadsheet_data(self):
+        # Authorization = {
+        #     "Authorization": str(AUTHORIZATION),
+        #     "username": os.getenv("username"),
+        #     "password": os.getenv("password")
+        # }
+        response = requests.get(POST_SHEETY)
+        #response.raise_for_status()
+        print(response.text)
+        pprint(response.text)
+
+    def get_prices(self):
+        response = requests.get(POST_SHEETY)
+        return response.json()["prices"]
--- main.py
+++ main.py
@@ -1,1 +1,69 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+
+import data_manager
+
+data_manager = data_manager.DataManager()
+
+#sheet_data = data_manager.get_prices()
+# Hardcoding response from Sheety's API for the time being for testing purposes
+
+sheet_data = [
+    {
+      "city": "Paris",
+      "iataCode": "",
+      "lowestPrice": 54,
+      "id": 2
+    },
+    {
+      "city": "Frankfurt",
+      "iataCode": "",
+      "lowestPrice": 42,
+      "id": 3
+    },
+    {
+      "city": "Tokyo",
+      "iataCode": "",
+      "lowestPrice": 485,
+      "id": 4
+    },
+    {
+      "city": "Hong Kong",
+      "iataCode": "",
+      "lowestPrice": 551,
+      "id": 5
+    },
+    {
+      "city": "Istanbul",
+      "iataCode": "",
+      "lowestPrice": 95,
+      "id": 6
+    },
+    {
+      "city": "Kuala Lumpur",
+      "iataCode": "",
+      "lowestPrice": 414,
+      "id": 7
+    },
+    {
+      "city": "New York",
+      "iataCode": "",
+      "lowestPrice": 240,
+      "id": 8
+    },
+    {
+      "city": "San Francisco",
+      "iataCode": "",
+      "lowestPrice": 260,
+      "id": 9
+    },
+    {
+      "city": "Dublin",
+      "iataCode": "",
+      "lowestPrice": 378,
+      "id": 10
+    }
+  ]
+
+for dict in sheet_data:
+    if dict["iataCode"] == None or dict["iataCode"] == "":
+        print("empty")
